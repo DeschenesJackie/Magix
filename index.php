76161b2d88f4ebbdedfe04082ff85bc2e061dd86
Deschênes Jackie <1704936@decinfo.cvm>
2020-02-28 14:08:57 -0500

    FinSession1

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
-require_once("partial/header.php");
 
+$titre = "Magix - Login";
+require_once("partial/header.php");
 ?>
+
+<main id="mainLogin">
+	<div id="loginPlayArea">
+		<div class="zoneEntre">
+			<form action="index.php" method="post">
+				<div class="form-label">
+					<label for="username">Nom d'usager : </label>
+				</div>
+				<div class="form-input">
+					<input type="text" mame="username" id="username" />
+				</div>
+				<div class="form-label">
+					<label for="password">Mot de passe : </label>
+				</div>
+				<div class="form-input">
+					<input type="text" mame="password" id="password" />
+				</div>
+				<div class="form-input">
+					<button type="submit">se Connecter</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</main>
+
 
 <?php
 
