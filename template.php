f7f19bbe8f1ea0914b9ba23769650af3787a6437
unknown <inspirate22@gmail.com>
2020-03-16 00:30:58 -0400

    ajout dossier5

--- template.php
+++ template.php
@@ -4,7 +4,7 @@
 ?>
 <div class="carte">
 	<div class="CarteSurface"></div>
-	<img class="zoneSprite" src="image/Andariel_(Diablo_II).gif" alt="Image du monstre">
+	<img class="zoneSprite" src="image/carteMonster/Andariel_(Diablo_II).gif" alt="Image du monstre">
 	<div class="zoneMechanic">Taunt</div>
 	<div class="zoneVie">5</div>
 	<div class="zoneAttaque">6</div>
