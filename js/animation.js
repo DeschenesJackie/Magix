b510fd18a9deda45875db2c0cfc91182dbaf56f2
unknown <inspirate22@gmail.com>
2020-04-21 23:44:37 -0400

    temp

--- js/animation.js
+++ js/animation.js
@@ -6,8 +6,8 @@ window.addEventListener("load", () => {
 	ctx = canvas.getContext("2d");
 	canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-	spriteList.push(new Shaman(100, 100, 7));
-
+	spriteList.push(new Shaman(canvas.width*0.2, canvas.height*0.2, 1));
+	spriteList.push(new Fallen(0, 0, 3));
 	tick();
 });
 
@@ -16,6 +16,33 @@ const tick = () => {
 
 	for (let i = 0; i < spriteList.length; i++) {
 		spriteList[i].tick();
+		if (spriteList[i] instanceof Shaman) {
+			if (spriteList[i].x < canvas.width*0.1 && spriteList[i].y < canvas.height*0.5) {
+				spriteList[i].setDirection(0);
+			}
+			if (spriteList[i].x < canvas.width*0.1 && spriteList[i].y > canvas.height*0.7) {
+				spriteList[i].setDirection(7);
+			}
+			if (spriteList[i].x > canvas.width*0.1 && spriteList[i].y > canvas.height*0.8) {
+				spriteList[i].setDirection(6);
+			}
+			if (spriteList[i].x > canvas.width*0.8 && spriteList[i].y > canvas.height*0.8) {
+				spriteList[i].setDirection(5);
+			}
+			if (spriteList[i].x > canvas.width*0.9 && spriteList[i].y < canvas.height*0.8) {
+				spriteList[i].setDirection(4);
+			}
+			if (spriteList[i].x > canvas.width*0.9 && spriteList[i].y < canvas.height*0.3) {
+				spriteList[i].setDirection(3);
+			}
+			if (spriteList[i].x < canvas.width*0.9 && spriteList[i].y < canvas.height*0.2) {
+				spriteList[i].setDirection(2);
+			}
+			if (spriteList[i].x < canvas.width*0.2 && spriteList[i].y < canvas.height*0.2) {
+				spriteList[i].setDirection(1);
+			}
+		}
+
 	}
 
 	window.requestAnimationFrame(tick);
--- js/sprite/shaman.js
+++ js/sprite/shaman.js
@@ -32,8 +32,8 @@ class Shaman {
 				this.speedY = -0.5;
 				break;
 			case 4:
-				this.speedX = -1;
-				this.speedY = 0;
+				this.speedX = 0;
+				this.speedY = -1;
 				break;
 			case 5:
 				this.speedX = 0.5;
@@ -46,6 +46,70 @@ class Shaman {
 			case 7:
 				this.speedX = 0.5;
 				this.speedY = 0.5;
+		}
+	}
+
+	tick() {
+		this.nX = this.x + this.speedX;
+		this.nY = this.y + this.speedY;
+
+		this.x = this.nX;
+		this.y = this.nY;
+
+		this.sprite.tick(ctx, this.x, this.y)
+
+		return true;
+	}
+}
+
+class Fallen {
+	constructor(x, y, direction) {
+		this.sprite = new TiledImage("image/FallenWalk.png", 10, 8, 100, true, 1.0);
+
+		this.x = x;
+		this.y = y;
+
+		this.setDirection(direction);
+
+	}
+
+	setDirection(dir) {
+
+		this.sprite.changeRow(dir)
+		this.sprite.changeMinMaxInterval(1, 10);
+
+		switch(dir) {
+			case 0:
+				this.speedX = 0;
+				this.speedY = 1;
+				break;
+			case 1:
+				this.speedX = 0.5;
+				this.speedY = -0.5;
+				break;
+			case 2:
+				this.speedX = 1;
+				this.speedY = 0;
+				break;
+			case 3:
+				this.speedX = 0.5;
+				this.speedY = 0.5;
+				break;
+			case 4:
+				this.speedX = 0;
+				this.speedY = -1;
+				break;
+			case 5:
+				this.speedX = -0.5;
+				this.speedY = 0.5;
+				break;
+			case 6:
+				this.speedX = -1;
+				this.speedY = 0;
+				break;
+			case 7:
+				this.speedX = -0.5;
+				this.speedY = -0.5;
 		}
 	}
 
